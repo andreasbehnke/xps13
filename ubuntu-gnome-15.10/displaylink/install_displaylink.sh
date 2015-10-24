56d752ff042a0f96ca0ed4ff5465180a86b8a3f5
Andreas Behnke <ab@andreasbehnke.com>
2015-10-24 22:20:31 +0200

    display link installer downloads artefact, patched installer script for systemd ubunt 15.10

--- ubuntu-gnome-15.10/displaylink/install_displaylink.sh
+++ ubuntu-gnome-15.10/displaylink/install_displaylink.sh
@@ -1,6 +1,12 @@
 # install patched displaylink driver and service
+mkdir tmp
+cd tmp
+wget http://downloads.displaylink.com/publicsoftware/DisplayLink-Ubuntu-1.0.138.zip
+unzip DisplayLink-Ubuntu-1.0.138.zip
+chmod +x displaylink-driver-1.0.138.run
 ./displaylink-driver-1.0.138.run --noexec --keep
-cp displaylink-installer.sh displaylink-driver-1.0.138/
+cp ../displaylink-installer.sh.patched displaylink-driver-1.0.138/
 cd displaylink-driver-1.0.138
-sudo ./displaylink-installer.sh install
-cd ..
+sudo ./displaylink-installer.sh.patched install
+cd ../..
+rm -R tmp
