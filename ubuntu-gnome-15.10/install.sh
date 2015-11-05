3103e6bcdc5b4bb50329c133a4ead6095717b4fd
Andreas Behnke <ab@andreasbehnke.com>
2015-11-05 18:37:37 +0100

    remove downloaded artefacts & uninstall thermald

--- ubuntu-gnome-15.10/install.sh
+++ ubuntu-gnome-15.10/install.sh
@@ -15,12 +15,12 @@ sudo update-initramfs -u
 sudo apt-get -y install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+rm google-chrome*.deb
 
 # power management
 sudo apt-get -y install powertop tlp tlp-rdw
+# >> echo 'auto' > '/sys/bus/usb/devices/1-4/power/control';
 
 # fixes for usb3 dell d3100 port replicator
 # disable intel powerclamp
-sudo rmmod intel_powerclamp 
-sudo echo install intel_powerclamp /bin/true > intel_powerclamp.conf
 sudo apt-get remove thermald
