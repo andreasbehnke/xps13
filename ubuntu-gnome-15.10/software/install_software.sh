2fc296eacace9fc92ea7a446f86523f94c028486
Andreas Behnke <ab@andreasbehnke.com>
2015-11-06 08:35:41 +0100

    Added installation of java

---
+++ ubuntu-gnome-15.10/software/install_software.sh
@@ -0,0 +1,6 @@
+# Install all required software for developer workstation
+
+# Install java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
