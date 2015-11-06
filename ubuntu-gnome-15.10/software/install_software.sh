e6cc026beffc0f5d9b18d01ae6dd874b675d4b13
Andreas Behnke <ab@andreasbehnke.com>
2015-11-06 08:36:17 +0100

    Added installation of virtualbox

--- ubuntu-gnome-15.10/software/install_software.sh
+++ ubuntu-gnome-15.10/software/install_software.sh
@@ -4,3 +4,9 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+
+# Install virtual box
+sudo echo 'deb http://download.virtualbox.org/virtualbox/debian wily contrib'  | sudo tee --append /etc/apt/sources.list
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install dkms virtualbox-5.0 -y
