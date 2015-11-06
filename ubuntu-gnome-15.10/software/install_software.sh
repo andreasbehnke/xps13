25d68d3f56c0dd0e7d47a9ff35eb726c264bc511
Andreas Behnke <ab@andreasbehnke.com>
2015-11-06 08:52:10 +0100

    Added installation for virtualbox (from original wily repository) and vagrant

--- ubuntu-gnome-15.10/software/install_software.sh
+++ ubuntu-gnome-15.10/software/install_software.sh
@@ -1,12 +1,9 @@
-# Install all required software for developer workstation
+# install all required software for developer workstation
 
-# Install java
+# install java 8 repository
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
 
-# Install virtual box
-sudo echo 'deb http://download.virtualbox.org/virtualbox/debian wily contrib'  | sudo tee --append /etc/apt/sources.list
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install dkms virtualbox-5.0 -y
+# install software
+sudo apt-get install dkms virtualbox vagrant oracle-java8-installer -y
+
