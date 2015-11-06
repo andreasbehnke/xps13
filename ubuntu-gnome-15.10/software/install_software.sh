2e57ce39603b17591483ca2cf29e83bd4b9e2ec0
Andreas Behnke <ab@andreasbehnke.com>
2015-11-06 12:58:47 +0100

    added openvpn to list of required software

--- ubuntu-gnome-15.10/software/install_software.sh
+++ ubuntu-gnome-15.10/software/install_software.sh
@@ -5,9 +5,10 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 # install software
+# - openvpn
 # - virtualbox
 # - vagrant
 # - java 8
 # - postgres client
 # - pgadmin
-sudo apt-get install dkms virtualbox vagrant oracle-java8-installer postgresql-client pgadmin3 -y
+sudo apt-get install openvpn dkms virtualbox vagrant oracle-java8-installer postgresql-client pgadmin3 -y
