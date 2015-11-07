44580e42d805ffa958be8b313865076e9ce8bbbd
andreasbehnke <ab@andreasbehnke.com>
2015-11-07 19:30:38 +0100

    Added comment for sleep command and moved sleep to top

--- ubuntu-gnome-15.10/powersave.sh
+++ ubuntu-gnome-15.10/powersave.sh
@@ -2,14 +2,14 @@
 #location: /usr/local/bin/powersave
 
 logger "triggered script powersave with status $1"
+# sleep 5 seconds for devices being ready after booting into system
+sleep 5
 
 case $1 in
 	false)
-		sleep 5
 		echo '600' > /sys/class/backlight/intel_backlight/brightness 
 		;;
 	true)
-		sleep 5
 		# the following settings are recommended by powertop
 		echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs';
 		echo 'auto' > '/sys/bus/usb/devices/1-4/power/control';
