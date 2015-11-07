e3e0d258e7dbc599cb85cd537b2f31bb83563432
Andreas Behnke <ab@andreasbehnke.com>
2015-11-07 19:27:44 +0100

    Wait 5 seconds before setting brightness and powersaving functions. This is a workaround for some devices being not ready when script runs after booting.

--- ubuntu-gnome-15.10/powersave.sh
+++ ubuntu-gnome-15.10/powersave.sh
@@ -5,13 +5,15 @@ logger "triggered script powersave with status $1"
 
 case $1 in
 	false)
+		sleep 5
 		echo '600' > /sys/class/backlight/intel_backlight/brightness 
 		;;
 	true)
-		# dimmed backlight		
-		echo '47' > /sys/class/backlight/intel_backlight/brightness 
+		sleep 5
 		# the following settings are recommended by powertop
 		echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs';
 		echo 'auto' > '/sys/bus/usb/devices/1-4/power/control';
+		# dimmed backlight
+		echo '47' > /sys/class/backlight/intel_backlight/brightness
 		;;
 esac
