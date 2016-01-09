a23b8e4cd81e8ab0e51644717f4943a402a23bdc
Andreas Behnke <ab@andreasbehnke.com>
2016-01-09 11:47:09 +0100

    Implemented installer for new version 1.0.335 which does not need special power handling on my xps13. Also the patch for kernel version check is not required, because installer only throws warning if kernel is not supported.

--- ubuntu-gnome-15.10/displaylink/install_displaylink.sh
+++ ubuntu-gnome-15.10/displaylink/install_displaylink.sh
@@ -1,19 +1,12 @@
 # install preconditions
 sudo apt-get install -y dkms
 
-# install patched displaylink driver and service
+# install displaylink driver and service, since version 1.0.335 no patch is required any more
 mkdir tmp
 cd tmp
-wget http://downloads.displaylink.com/publicsoftware/DisplayLink-Ubuntu-1.0.138.zip
-unzip DisplayLink-Ubuntu-1.0.138.zip
-chmod +x displaylink-driver-1.0.138.run
-./displaylink-driver-1.0.138.run --noexec --keep
-cp ../displaylink-installer.sh.patched displaylink-driver-1.0.138/
-cd displaylink-driver-1.0.138
-sudo ./displaylink-installer.sh.patched install
-cd ../..
+wget -O DisplayLink_Ubuntu_1.0.335.zip http://www.displaylink.com/downloads/file?id=123
+unzip DisplayLink_Ubuntu_1.0.335.zip
+chmod +x displaylink-driver-1.0.335.run
+sudo ./displaylink-driver-1.0.335.run
+cd ..
 rm -R tmp
-
-# install powersaving script, turn off displaylink service on power disconnect
-sudo cp displaylink-powersave.sh /usr/local/bin
-sudo cp 80-displaylink-powersave.rules /etc/udev/rules.d
--- ubuntu-gnome-15.10/displaylink/uninstall_displaylink.sh
+++ ubuntu-gnome-15.10/displaylink/uninstall_displaylink.sh
@@ -1,16 +1,9 @@
 # uninstall patched displaylink driver and service
 mkdir tmp
 cd tmp
-wget http://downloads.displaylink.com/publicsoftware/DisplayLink-Ubuntu-1.0.138.zip
-unzip DisplayLink-Ubuntu-1.0.138.zip
-chmod +x displaylink-driver-1.0.138.run
-./displaylink-driver-1.0.138.run --noexec --keep
-cp ../displaylink-installer.sh.patched displaylink-driver-1.0.138/
-cd displaylink-driver-1.0.138
-sudo ./displaylink-installer.sh.patched uninstall
-cd ../..
+wget -O DisplayLink_Ubuntu_1.0.335.zip http://www.displaylink.com/downloads/file?id=123
+unzip DisplayLink_Ubuntu_1.0.335.zip
+chmod +x displaylink-driver-1.0.335.run
+sudo ./displaylink-driver-1.0.335.run uninstall
+cd ..
 rm -R tmp
-
-# uninstall powersaving script
-sudo rm /usr/local/bin/displaylink-powersave.sh
-sudo rm  /etc/udev/rules.d/80-displaylink-powersave.rules
